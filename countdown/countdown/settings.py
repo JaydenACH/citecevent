62b13b416a6ccf12d3ba5b985c34a2c6042de719
JaydenAngCH <angchunhang@gmail.com>
2022-11-02 16:42:14 +0800

    update settings.py

--- countdown/countdown/settings.py
+++ countdown/countdown/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['citeceventcountdown.herokuapp.com','127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://*.citeceventcountdown.herokuapp.com']
 
 # Application definition
 
